b1532b3958c02a1cc2fe0021687c0e147685dde0
Uchinomaru <bravecred@gmail.com>
2021-01-16 23:17:53 +0900

    Add：sessionを使用するための設定を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end

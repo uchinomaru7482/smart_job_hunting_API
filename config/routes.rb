e252c9a5bf29b4b2e75cf7daec1fda144fabb5b0
Uchinomaru <bravecred@gmail.com>
2021-01-16 23:20:23 +0900

    Add：sessionに関するルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   namespace "api" do
     namespace "v1" do
+      get "/logged_in", to: "sessions#logged_in"
+
       resources :users
       resources :companys
+      resources :sessions
     end
   end
 end

62b7e3ebd3ac5cae674d6edec2d6c3f88dc07102
Uchinomaru <bravecred@gmail.com>
2021-01-19 08:56:34 +0900

    進捗状況をenumで管理する

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,4 @@
 class Company < ApplicationRecord
+  enum progress: { consideration: 0, entry: 1, primary_interview: 2, second_interview: 3, final_interview: 4, unofficial_offer: 5, prayer: 6 }
   belongs_to :user
 end

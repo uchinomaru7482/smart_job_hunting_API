ecf99132c4175c4d5e5277feb645d4448b5ad8d8
Uchinomaru <bravecred@gmail.com>
2021-01-19 10:58:54 +0900

    Add：companyモデルのバリデーションを追加

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,8 @@
 class Company < ApplicationRecord
   enum progress: { consideration: 0, entry: 1, primary_interview: 2, second_interview: 3, final_interview: 4, unofficial_offer: 5, prayer: 6 }
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :name, presence: true
+  validates :progress, numericality: { less_than_or_equal_to: 6 }
 end

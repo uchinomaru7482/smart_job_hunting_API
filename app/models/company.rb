41f80f4b0991a46d63401f6c7031f604ac7d895e
Uchinomaru <bravecred@gmail.com>
2021-01-27 12:46:57 +0900

    Fix：enumの値を日本語に変更

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,8 +1,7 @@
 class Company < ApplicationRecord
-  enum progress: { consideration: 0, entry: 1, primary_interview: 2, second_interview: 3, final_interview: 4, unofficial_offer: 5, prayer: 6 }
+  enum progress: { 応募検討: 0, エントリー: 1, 一次面接: 2, 二次面接: 3, 最終面接: 4, 内定: 5, お祈り: 6 }
   belongs_to :user
 
   validates :user_id, presence: true
   validates :name, presence: true
-  validates :progress, numericality: { less_than_or_equal_to: 6 }
 end
